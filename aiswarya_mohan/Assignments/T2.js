f44690a6a8a84a0f69da33d362c82019b504b1b7
a-123-m <aiswarya2000mohan@gmail.com>
2024-07-02 23:07:14 +0530

    assignment work - July 2

---
+++ aiswarya_mohan/Assignments/T2.js
@@ -0,0 +1,11 @@
+let rows=5
+let pattern=""
+for(i=1;i<=rows;i++)
+    {
+        for(j=1;j<=i;j++)
+            {
+                pattern+="*"
+            }
+            pattern+="\n"
+            console.log(pattern)
+    }
