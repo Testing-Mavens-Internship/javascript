6932156bd6637f7d203d68269c793608b0efee68
Aswin <aswinkrishnaa>
2024-07-02 23:42:27 +0530

    vowel check,grade check, odd or even

---
+++ aswin/assignment1/vowelcheck.js
@@ -0,0 +1,14 @@
+let letter = "i";
+
+switch (letter) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        console.log(letter + " is a vowel");
+        break;
+    default:
+        console.log(letter + " is not a vowel");
+        break;
+}
