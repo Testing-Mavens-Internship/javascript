7a3d23bcc5c495956b7c0faae200d010fb9ac84f
unknown <sidharthks255@gmail.com>
2024-07-02 23:10:04 +0530

    Assignments

---
+++ sidharth/Assignment/T2.js
@@ -0,0 +1,12 @@
+let n=5;
+let m=1;
+while(m<=n){
+    let a=1;
+    let pattern='';
+    while(a<=m){
+        pattern+="*";
+        a++
+    }
+    console.log(pattern);
+    m++;
+}
---
+++ sidharth/Assignment/Vowels.js
@@ -0,0 +1,18 @@
+ let letter='A';
+ switch(letter){
+  case 'a':
+  case 'e':
+  case'i':
+  case'o':
+  case 'u':
+  case 'A':
+  case 'E':
+  case'I':
+  case'O':
+  case 'U':
+  console.log(letter + 'is vowel');
+  break;
+  default:
+  console.log(letter + 'is not vowel');
+        
+}
