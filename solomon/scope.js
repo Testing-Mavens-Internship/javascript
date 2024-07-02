bcaa57ff3532b55086a19bdac2b0bcbbda87e85e
Solomon Joseph <solomon243joseph@gmail.com>
2024-07-02 11:54:12 +0530

    New file

--- solomon/scope.js
+++ solomon/scope.js
@@ -1,9 +1,44 @@
 let x=5;
 var v="hai";
-const n="Solomon"
+const n="Solomon";
 {
     console.log(x);
     console.log(v);
     console.log(n);
 }
 
+
+let y;//can declare variable any where and we can assign value any time
+var w;
+const z="Solomon";//should declare when initialising itself
+y=7;
+w=12;
+{
+    console.log(y);
+    console.log(w);
+    console.log(z);
+}
+
+//console.log(test);
+let test=12;
+console.log(a);
+var a=23
+//console.log(e);
+//const e;
+
+var te=12;
+var te=100;
+console.log(te);
+
+//let te=13;
+//let te=101;
+//console.log(te);
+
+b=6;
+c=1;
+console.log(1&&3)
+console.log(1||6)
+console.log("b")
+console.log(6||1)
+console.log(!b)
+console.log(!c)
