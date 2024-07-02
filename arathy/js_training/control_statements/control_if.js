35bba4d7e2dbbcc633ed142be433039cddc6e6a9
Arathy17 <arathyudayakumar17@gmail.com>
2024-07-02 17:10:19 +0530

    This folder contains all the activities, except for assignments given, that were completed during today's session.

---
+++ arathy/js_training/control_statements/control_if.js
@@ -0,0 +1,42 @@
+// x=16;
+// y=18;
+// if(x<y)
+//     {
+//         if(x>0)
+//             {
+//         console.log(y+" is greater")
+//             }
+//         }
+// x=10;
+// y=15;
+// if(x<y)
+//     {
+//         console.log(y+"is greater")
+//     }
+//     else if(x>y)
+//         {
+//         console.log(x+"is greater")
+
+//     }
+
+
+// var a=2;
+// switch(a)
+// {
+//     case 1:
+//         console.log("one")
+//         break;
+//     case 2:
+//         console.log("two")
+//         break;
+//     default:
+//         console.log("default")
+// }
+// for(i=0;i<=10;i++){
+//     for(j=0;j<=i;j++){
+//         console.log(j,i)
+//     }
+// }
+
+
+
---
+++ arathy/js_training/let_const_var.js
@@ -0,0 +1,15 @@
+let x
+
+console.log(x)
+var y
+
+console.log(y)
+const z=2
+console.log(z)
+
+let p=2
+console.log(p)
+var u=6
+console.log(u)
+const e=5
+console.log(e)
