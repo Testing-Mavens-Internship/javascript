70ceadf054a48e45ef32bcac4c9c615159da21ac
unknown <dinujoy2001@gmail.com>
2024-07-02 17:16:27 +0530

    In todays session(2/7/2024) we discussed about basics of javascript including data type, variables,Hoisting, different control statements,loops and this folder contain examples that i tried during the session.

---
+++ dinu/training/control.js
@@ -0,0 +1,71 @@
+
+/*
+//simple if
+
+var x=-2;
+if (x>0){
+    console.log(x+' is a positive number');
+}
+
+//if else
+
+var x=-2;
+if (x>0){
+    console.log(x+' is a positive number');
+}
+else{
+    console.log(x+' is a negative number');
+}
+
+//nested if
+
+var x = 2;
+var y = 3;
+if (x>y){
+    if(x>0){
+        console.log(x+' is positive');
+    }
+    else{
+        console.log(x+' is negative');
+    }
+}
+
+//else if ladder
+
+var x = 2;
+if (x>0){
+    console.log(x+ ' is a positive');
+}
+    else if (x<0){
+    console.log(x+ 'is a negative');
+    }
+    else{
+    console.log(x+' is zero');
+    }
+
+//switch
+
+var a = 5;
+switch (a){
+    case 1:
+        console.log("one");
+        break;
+    case 2:
+        console.log("two");
+        break;
+    case 3:
+        console.log("three");
+        break;
+    default:
+        console.log("default")    
+}*/
+ 
+//nested for
+
+for(let i=0;i<10;i++){
+    for(let j=0;j<=i;j++){
+        console.log(j,i);
+    }
+}
+      
+
---
+++ dinu/training/logic.js
@@ -0,0 +1,8 @@
+a=6
+b=1
+console.log(a&&b)
+console.log(a||b)
+console.log(!a)
+console.log(!b)
+console.log(3&&3)
+console.log(4||6)
