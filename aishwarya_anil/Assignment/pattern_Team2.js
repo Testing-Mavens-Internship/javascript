e965c532c08d6ff49e87edd4a7b9a0ae352d559d
AISWARYA ANILKUMAR <aiswaryaanilkumar33@gmail.com>
2024-07-02 19:09:08 +0530

    july2

---
+++ aishwarya_anil/Assignment/pattern_Team2.js
@@ -0,0 +1,75 @@
+
+{
+let i=1;
+let n=5;
+while(i<=n) {
+
+    let pattern='';
+    let j=1;
+    while(j<=i)
+        {
+            pattern+="*";
+            j++;
+        }
+        console.log(pattern);
+        i++;   
+}
+
+}
+
+{
+let i=1;
+
+let num=5;
+while(i<=num) {
+
+    let pattern='';
+    let j=1;
+    while(j<=i)
+        {
+            pattern+=i;
+            j++;
+        }
+        console.log(pattern);
+        i++;   
+}
+
+let numb=5;
+let pattern = "";
+for (let i = 1; i <numb; i++) {
+   for (let j =i; j <numb; j++) {
+      pattern +=i+5;
+   }
+   pattern += "\n";
+}
+console.log(pattern);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
