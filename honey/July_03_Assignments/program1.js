e70350f89c64a58304e0c602e663b460abd51460
Hanan@FathimaHanan <fathimahanan9@gmail.com>
2024-07-04 01:12:44 +0530

    03/07/2024 Assignments

---
+++ honey/July_03_Assignments/program1.js
@@ -0,0 +1,6 @@
+// Description: Given a nested array of numbers, flatten the array and calculate the sum of all the numbers.
+// const nestedArray = [[1, 2, [3, 4]], [5, [6, [7, 8]]], 9];
+// O/P : 45
+
+const nestedArray = [[1, 2, [3, 4]], [5, [6, [7, 8]]], 9];
+console.log(nestedArray.flat(Infinity).reduce((sum,item)=>sum+item));
---
+++ honey/July_03_Assignments/program2.js
@@ -0,0 +1,21 @@
+// Description: Given an array of people objects, group them by age.
+ 
+// const people = [
+//   { name: "Alice", age: 25 },
+//   { name: "Bob", age: 25 },
+//   { name: "Charlie", age: 30 },
+//   { name: "David", age: 35 },
+//   { name: "Eve", age: 30 }
+// ];
+ 
+// OP :  [["25", ["Alice", "Bob"]], ["30", ["Charlie", "Eve"]], ["35", ["David"]]]
+
+const people = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 25 },
+    { name: "Charlie", age: 30 },
+    { name: "David", age: 35 },
+    { name: "Eve", age: 30 }
+  ];
+
+  
