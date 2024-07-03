c451cac00bd63ed3d05170ccb7732f1b364a48e2
H2K2004 <kkharikrishnan2004@gmail.com>
2024-07-03 08:20:52 +0530

    Assignments-02/07/24

---
+++ krishna/Assignment_01/Butterfly.js
@@ -0,0 +1,44 @@
+for (let i = 1; i <= 4; i++) //4 is the size
+    {
+    let pattern= '';
+    for (let j = 1; j <= i; j++) 
+        {
+        pattern += '*';
+    }
+    let space='';
+    for (let j = 1; j <= 2 *(4-i); j++) 
+        {
+        space += ' ';
+    }
+    let star='';
+    for (let j = 1; j <= i; j++) 
+        {
+        star += '*';
+    }
+    
+    console.log(pattern+space+star);
+}
+
+
+for (let i = 4; i >= 1; i--) 
+    {
+    let pattern = '';
+    let space='';
+    let star='';
+    for (let j = 1; j <= i; j++) 
+        {
+        pattern += '*';
+    }
+  
+    for (let j = 1; j <= 2 * (4-i); j++) 
+        {
+        space += ' ';
+    }
+ 
+    for (let j = 1; j <= i; j++) 
+        {
+        star += '*';
+    }
+    
+    console.log(pattern+space+star);
+}
