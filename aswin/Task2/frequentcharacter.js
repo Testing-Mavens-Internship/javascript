a9de339571306aa393b923c1349b3bd47818fe9d
Aswin <aswinkrishnaa>
2024-07-04 10:24:45 +0530

    find the frequent character from a string

--- aswin/Task2/frequentcharacter.js
+++ aswin/Task2/frequentcharacter.js
@@ -1,15 +1,12 @@
 const input = "abbcccddddeeeee";
 let chars = input.split("");
-let counts = {};
-chars.forEach(char => {
-    counts[char] = (counts[char] || 0) + 1;
+let counts={};
+chars.forEach(char=>{
+    counts[char]=(counts[char]||0)+1;
 });
-let mcount = 0;
-let mostfrequentChar = '';
-Object.keys(counts).forEach(char => {
-    if (counts[char] > mcount) {
-        mostfrequentChar = char;
-        mcount =counts[char]
-    }
+let mcount=0;
+let mostfrequentChar='';
+Object.keys(counts).forEach(char=>{
+    mcount=counts[char]>mcount?(mostfrequentChar=char,counts[char]):mcount;
 });
 console.log(mostfrequentChar);
