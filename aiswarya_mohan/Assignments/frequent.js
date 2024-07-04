f11711003b4617f7243b77f161cc5de0e2ddb3a5
a-123-m <aiswarya2000mohan@gmail.com>
2024-07-04 09:53:40 +0530

    assignment work

---
+++ aiswarya_mohan/Assignments/frequent.js
@@ -0,0 +1,18 @@
+const str="abbcccddddeeeee"
+let chars=str.split('')
+console.log(chars)
+let counts={}
+chars.forEach(char => {
+    counts[char] = (counts[char] || 0)+1
+})
+console.log(counts)
+let mcount=0
+let mostfreqchar=''
+Object.keys(counts).forEach(char =>{
+    if(counts[char]>mcount)
+    {
+        mostfreqchar=char
+        mcount = counts[char]
+    }
+})
+console.log(mostfreqchar)
