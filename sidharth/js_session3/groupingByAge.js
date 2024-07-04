4c22a18f768d2ddc04679774477ee352044e06c0
unknown <sidharthks255@gmail.com>
2024-07-04 10:05:41 +0530

    test

---
+++ sidharth/js_session3/groupingByAge.js
@@ -0,0 +1,10 @@
+const people = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 25 },
+    { name: "Charlie", age: 30 },
+    { name: "David", age: 35 },
+    { name: "Eve", age: 30 }
+  ];
+  
+  const groupedByAge = people.reduce() 
+  
