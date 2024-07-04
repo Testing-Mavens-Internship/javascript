1c20d30e3ffa938a6bbb0fbd5e9581706f625627
Hanan@FathimaHanan <fathimahanan9@gmail.com>
2024-07-04 11:48:48 +0530

    Array of Objects,group by age

--- honey/July_03_Assignments/program2.js
+++ honey/July_03_Assignments/program2.js
@@ -10,12 +10,23 @@
  
 // OP :  [["25", ["Alice", "Bob"]], ["30", ["Charlie", "Eve"]], ["35", ["David"]]]
 
-const people = [
-    { name: "Alice", age: 25 },
-    { name: "Bob", age: 25 },
-    { name: "Charlie", age: 30 },
-    { name: "David", age: 35 },
-    { name: "Eve", age: 30 }
-  ];
+const peoples = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 25 },
+  { name: "Charlie", age: 30 },
+  { name: "David", age: 35 },
+  { name: "Eve", age: 30 }
+];
 
-  
+const grouped = peoples.reduce(groupPeople, {});
+const result = Object.entries(grouped);
+console.log(result)
+
+function groupPeople(acc,people)
+{
+    if (!acc[people.age]) {
+        acc[people.age] = [];
+    }
+    acc[people.age].push(people.name);
+    return acc
+}
