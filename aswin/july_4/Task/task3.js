0325979d1f8b907ae41d8d2fdd63e388f33223ce
Aswin <aswinkrishnaa>
2024-07-05 10:57:45 +0530

    task3

--- aswin/july_4/Task/task3.js
+++ aswin/july_4/Task/task3.js
@@ -34,30 +34,6 @@ class Inventory {
         console.log(`Added product: ${product.getName()}`);
     }
 
-    removeProduct(productName) {
-        const index = this.products.findIndex(product => product.getName() === productName);
-        index > -1
-            ? (this.products.splice(index, 1), console.log(`Removed product: ${productName}`))
-            : console.error(`Product ${productName} not found.`);
-    }
-
-    updateProduct(productName, newDetails) {
-        const product = this.products.find(product => product.getName() === productName);
-        product
-            ? (newDetails.name !== undefined && product.setName(newDetails.name),
-               newDetails.price !== undefined && product.setPrice(newDetails.price),
-               newDetails.quantity !== undefined && product.setQuantity(newDetails.quantity),
-               console.log(`Updated product: ${productName}`))
-            : console.error(`Product ${productName} not found.`);
-    }
-
-    searchProduct(productName) {
-        const product = this.products.find(product => product.getName() === productName);
-        product
-            ? (console.log(`Found product: ${product.getName()} - Price: ${product.getPrice()} - Quantity: ${product.getQuantity()}`),
-               product)
-            : (console.error(`Product ${productName} not found.`), null);
-    }
 }
 
 const inventory = new Inventory();
@@ -68,10 +44,3 @@ const product2 = new Product("Smartphone", 700, 20);
 inventory.addProduct(product1);
 inventory.addProduct(product2);
 
-inventory.searchProduct("Laptop");
-
-inventory.updateProduct("Laptop", { price: 950, quantity: 8 });
-
-inventory.removeProduct("Smartphone");
-
-inventory.searchProduct("Smartphone");
