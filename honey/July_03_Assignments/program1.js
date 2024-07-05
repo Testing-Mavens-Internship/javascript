903c764b179b85ebc5ff3f7c294ead89466895b5
Hanan@FathimaHanan <fathimahanan9@gmail.com>
2024-07-05 12:26:34 +0530

    Committed

--- honey/July_03_Assignments/program1.js
+++ honey/July_03_Assignments/program1.js
@@ -3,4 +3,4 @@
 // O/P : 45
 
 const nestedArray = [[1, 2, [3, 4]], [5, [6, [7, 8]]], 9];
-console.log(nestedArray.flat(Infinity).reduce((sum,item)=>sum+item));
+console.log(nestedArray.flat(Infinity).reduce((sum,item)=>sum+item,0));
