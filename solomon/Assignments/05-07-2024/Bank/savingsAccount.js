7f46a15c44aa30108d020562b2f9bc1f6a8635a5
Solomon Joseph <solomon243joseph@gmail.com>
2024-07-05 21:14:11 +0530

    Today (05-07-2024) we were discussing about promise , async and await functions.

---
+++ solomon/Assignments/05-07-2024/Bank/savingsAccount.js
@@ -0,0 +1,16 @@
+import {BankAccount} from "./bankAccount.js";
+
+export class SavingsAccount extends BankAccount {
+    constructor(accountNumber, balance, interestRate) {
+      super(accountNumber, balance);
+      this.interestRate = interestRate;
+    }
+  
+    applyInterest() {
+      const interest = this.balance * (this.interestRate / 100);
+      this.balance += interest;
+      console.log(`Applied interest of ${interest} to account ${this.accountNumber}. New balance: ${this.balance}`);
+    }
+  }
+
+ 
